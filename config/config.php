a4bab7793dda55f56de1946f11e94a8003c4a7dc
Saugat Ghimire <31468281+prosgt@users.noreply.github.com>
2021-07-15 07:42:38 +0545

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+  // DB Params
+  define("DB_HOST", "localhost");
+  define("DB_USER", "root");
+  define("DB_PASS", "");
+  define("DB_NAME", "emall");
+  define('URL', 'http://hamroshop.lan/hamroshop');
+
+
+  // App Root
+ //
+ define('APPROOT', 'C:/xampp/htdocs/hamroshop/assest');
+
+ 
+ //C:\xampp\htdocs\hamroshop\assest
+ //define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  // Site Name
+  //define('SITENAME', 'SharePosts');
+  
---
+++ config/conn.php
@@ -0,0 +1,10 @@
+<?php
+include_once 'config.php';
+$con = mysqli_connect("DB_HOST","DB_USER","DB_PASS","DB_NAME");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
